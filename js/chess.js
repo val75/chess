0b8e937e2186f2b9fe311f9c618eafeb7cef2220
Valentin Ghiur <valentin@testinmotion.com>
2017-08-04 21:15:01 -0700

    Add cfg object with some initial properties.

--- js/chess.js
+++ js/chess.js
@@ -3,8 +3,13 @@
  */
 
 var
+    cfg = {
+        draggable: true,
+        dropOffBoard: 'snapback', // this is the default
+        position: 'start'
+    },
     init = function () {
-        var board = ChessBoard('board1', 'start');
+        var board = ChessBoard('board1', cfg);
     };
 
 $(document).ready(init);
