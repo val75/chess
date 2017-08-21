a3642cd74daf17e7373fc61c6914dda22973bc3a
Valentin Ghiur <valentin@testinmotion.com>
2017-08-21 09:04:34 -0700

    Add chess.js CDN (maybe not needed) Add onDragStart and verified it works.

--- js/chess.js
+++ js/chess.js
@@ -3,13 +3,26 @@
  */
 
 var
-    cfg = {
-        draggable: true,
-        dropOffBoard: 'snapback', // this is the default
-        position: 'start'
-    },
-    init = function () {
-        var board = ChessBoard('board1', cfg);
+    cfg, onDragStart, init;
+
+onDragStart = function (source, piece, position, orientation) {
+    console.log("Drag started:");
+    console.log("Source: " + source);
+    console.log("Piece: " + piece);
+    console.log("Orientation: " + orientation);
+    console.log("--------------------");
+};
+
+cfg = {
+    draggable: true,
+    position: 'start',
+    dropOffBoard: 'snapback', // this is the default
+    onDragStart: onDragStart
+};
+
+init = function () {
+    var game = new Chess();
+    var board = ChessBoard('board1', cfg);
     };
 
 $(document).ready(init);
